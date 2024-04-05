7be73421149484d947a32bc189563fec744b9c81
Owner <anulsasidharan@gmail.com>
2024-04-05 00:46:16 -0400

    Send email using the forum

--- send_email.py
+++ send_email.py
@@ -1,21 +1,19 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "anulscloud01@gmail.com"
-password = "mfrn nlzu nrro hleh"
-
-receiver = "anulscloud01@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi!
-How are you?
-Bye.
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
-
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "anulscloud01@gmail.com"
+    password = "mfrn nlzu nrro hleh"
+    receiver = "anulscloud01@gmail.com"
+    context = ssl.create_default_context()
+    # message = """\
+    # Subject: Hi!
+    # How are you?
+    # Bye.
+    # """
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
